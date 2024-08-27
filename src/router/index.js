44882fee278897a531a5c4667050c63b26eaf3be
https://github.com/cyberseris/Node.js_todolist_HW.git <giy>
2024-08-27 16:04:31 +0800

    新增 Not Found 頁面

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,10 @@ const router = createRouter({
     {
       path: '/todo',
       component: () => import('../views/TodoView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: () => import('../views/NotfoundView.vue')
     }
   ]
 })
--- src/views/NotfoundView.vue
+++ src/views/NotfoundView.vue
@@ -2,21 +2,10 @@
     <div id="loginPage" class="bg-yellow">
       <div class="conatiner loginPage vhContainer ">
         <div class="side">
-          <a href="#"><img class="logoImg" src="../../public/logo.png" alt=""></a>
-          <img class="d-m-n" src="../../public/workImg.png" alt="workImg">
+          <p>404 Not Found</p>
+          <p>您所造訪的網頁不存在...</p>
         </div>
-        <div>
-          <form class="formControls" action="index.html">
-            <h2 class="formControls_txt">最實用的線上代辦事項服務</h2>
-            <label class="formControls_label" for="email">Email</label>
-            <input class="formControls_input" type="text" id="email" name="email" placeholder="請輸入 email" v-model="account" required>
-            <label class="formControls_label" for="pwd">密碼</label>
-            <input class="formControls_input" type="password" name="pwd" id="pwd" placeholder="請輸入密碼" v-model="password" required>
-            <button class="formControls_btnSubmit"  @click.prevent="signIn">登入</button>
-            <!-- <a class="formControls_btnLink" href="#/register">註冊帳號</a> -->
-            <RouterLink class="formControls_btnLink" to="/register">註冊帳號</RouterLink>
-          </form>
-        </div>
+
       </div>
     </div>
 </template>
